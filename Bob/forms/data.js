a1717c6dd07826b0ca0357c35588bc977a4fb5d1
Bob Recio <bobrecio@gmail.com>
2021-01-21 19:16:21 -0600

    added data.js and main form template

---
+++ Bob/forms/data.js
@@ -0,0 +1,14 @@
+const emps = {
+    'name': {
+        'first':'Bob', 
+        'last':'Recio'
+    },
+    'address': {
+        'street': '313 Gen Hudnell',
+        'city':'San Antonio',
+        'state':'TX',
+        'zipcode':'78210'
+    },
+    'email': 'recioiii_bob@bah.com', 
+    'phone': '210-212-1212'
+};
