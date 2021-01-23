4601ef2f3f8ee331c1df2ea5ea0409f3ef283aa0
tate-steinour <61195987+tate-steinour@users.noreply.github.com>
2021-01-23 13:06:06 -0500

    Update lambdaToS3.py

--- Tate/Lambda-S3/lambdaToS3.py
+++ Tate/Lambda-S3/lambdaToS3.py
@@ -6,15 +6,14 @@ s3 = boto3.client('s3')
 def lambda_handler(event, context):
     
     body = json.loads(event['body'])
-    firstname = body['firstname']
-    lastname = body['lastname']
+    name = body['name']
     address = body['address']
     email = body['email']
     phone = body['phone']
     
     bucket = 'team3-docufiller'
     filename = 'info.json'
-    content = {'firstname': firstname, 'lastname': lastname, 'address': address, 'email': email, 'phone': phone}
+    content = {'name': name, 'address': address, 'email': email, 'phone': phone}
     payload = json.dumps(content)
 
     s3.put_object(Bucket=bucket, Key=filename, Body=payload)
