159d7ebe843e4adf9bbf8491e2ef8731573d34d9
Bob Recio <bobrecio@gmail.com>
2021-01-26 20:19:31 -0600

    Added working lambda index.js

---
+++ Bob/GoodLambda/index.js
@@ -0,0 +1,53 @@
+Response
+"code: 200"
+
+Function Logs
+"Recio",
+              fullAddress: "3133 Gen Hudnell, San Antonio, TX  78210",
+              street: "3133 Gen Hudnell",
+              city: "San Antonio",
+              state: "TX",
+              zip: "78210",
+              email: "recioiii_bob@bah.com",
+              phone: "210-212-1212"
+          }
+      })</script></body></html>
+2021-01-27T02:16:52.578Z	2b4b6155-e3ac-4835-94d3-1d3c2df56e68	INFO	<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Employee Form 02</title><script src="https://cdn.jsdelivr.net/npm/vue"></script><script src='data.js'></script><link rel="icon" href="data:;base64,iVBORw0KGgo="><style>body {font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;}table {background-color: #f0f0f0;width: 600px;border: 1px solid #000;}th.formTitle {background: #000;color: #ddd;text-align: left;padding: 5px 10px;font-size: 1em;text-shadow: #555 2px 2px;}td {background-color: #fff;color: black;vertical-align: top;padding: 2px 10px;font-size: 1.1em;}td.label {background: #ddd;color: #000;text-align: left;padding: 5px 10px;font-size: .8em;}div.formInfo {font-size: .5em;}</style></head><body><div id='dataForm'><table><tr><th class='formTitle' colspan=2>Mailing Address</th></tr><tr><td class='label' class=>Name</td class=><td>{{fullName}}</td></tr><tr><td class='label' class=>Address</td class=><td>{{street}}<br>{{city}}, {{state}}  {{zip}}</td></tr></table><div class='formInfo'>Form 02: Mailing Address</div><div id='printTime' class='formInfo'>Printed:  Wed Jan 27 2021 02:16:51 GMT+0000 (Coordinated Universal Time)</div><script>
+      const formData = new Vue({
+          el: "#dataForm",
+          data: {
+              empName: "Recio, Bob",
+              fullName: "Bob Recio",
+              fname: "Bob",
+              lname: "Recio",
+              fullAddress: "3133 Gen Hudnell, San Antonio, TX  78210",
+              street: "3133 Gen Hudnell",
+              city: "San Antonio",
+              state: "TX",
+              zip: "78210",
+              email: "recioiii_bob@bah.com",
+              phone: "210-212-1212"
+          }
+      })</script></body></html>
+2021-01-27T02:16:52.665Z	2b4b6155-e3ac-4835-94d3-1d3c2df56e68	INFO	<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Employee Form 03</title><script src="https://cdn.jsdelivr.net/npm/vue"></script><script src='data.js'></script><link rel="icon" href="data:;base64,iVBORw0KGgo="><style>body {font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;}table {background-color: #f0f0f0;width: 600px;border: 1px solid #000;}th.formTitle {background: #000;color: #ddd;text-align: left;padding: 5px 10px;font-size: 1em;text-shadow: #555 2px 2px;}td {background-color: #fff;color: black;vertical-align: top;padding: 2px 10px;font-size: 1.1em;}td.label {background: #ddd;color: #000;text-align: left;padding: 5px 10px;font-size: .8em;}div.formInfo {font-size: .5em;}</style></head><body><div id='dataForm'><table><tr><th class='formTitle' colspan='2' >Contact Methods</th></tr><tr><td class='label'>Employee</td><td>{{fullName}}</td></tr><tr><td class='label'>Email</td><td>{{email}}</td></tr><tr><td class='label'>Phone</td><td>{{phone}}</td></tr></table><div class='formInfo'>Form 03: Contact Methods</div><div id='printTime' class='formInfo'>Printed:  Wed Jan 27 2021 02:16:51 GMT+0000 (Coordinated Universal Time)</div><script>
+      const formData = new Vue({
+          el: "#dataForm",
+          data: {
+              empName: "Recio, Bob",
+              fullName: "Bob Recio",
+              fname: "Bob",
+              lname: "Recio",
+              fullAddress: "3133 Gen Hudnell, San Antonio, TX  78210",
+              street: "3133 Gen Hudnell",
+              city: "San Antonio",
+              state: "TX",
+              zip: "78210",
+              email: "recioiii_bob@bah.com",
+              phone: "210-212-1212"
+          }
+      })</script></body></html>
+END RequestId: 2b4b6155-e3ac-4835-94d3-1d3c2df56e68
+REPORT RequestId: 2b4b6155-e3ac-4835-94d3-1d3c2df56e68	Duration: 918.58 ms	Billed Duration: 919 ms	Memory Size: 128 MB	Max Memory Used: 92 MB
+
+Request ID
+2b4b6155-e3ac-4835-94d3-1d3c2df56e68
